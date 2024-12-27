088900fb18ed3cdbe8fbe739124bbbd1b4ee18d7
GrecoLucas <up202208296@up.pt>
2024-12-27 15:24:25 +0000

    Basic moviment

---
+++ data.pl
@@ -0,0 +1,30 @@
+board(1, [
+    [w, w, empty, empty, empty, empty, empty, empty],
+    [w, w, empty, empty, empty, empty, empty, empty],
+    [w, w, empty, empty, empty, empty, empty, empty],
+    [w, w, empty, empty, empty, empty, empty, empty],
+    [w, w, empty, empty, empty, empty, empty, empty],
+    [w, w, empty, empty, empty, empty, empty, empty],
+    [empty, empty, b, b, b, b, b, b],
+    [empty, empty, b, b, b, b, b, b]
+]).
+
+
+symbol(w, 'W').
+symbol(b, 'B').
+symbol(empty, '.').
+
+
+
+% -----------------------------------------------
+% 2. Players
+% -----------------------------------------------
+
+player(1, player1).
+player(2, player2).
+
+piece(player1, w).
+piece(player2, b).
+
+change_player(player1, player2).
+change_player(player2, player1).
--- game.pl
+++ game.pl
@@ -1,114 +1,120 @@
-% Onde o jogo começa chamada menu.
-% GameState representa o estado atual do jogo.
-% GameState é composto por (Current_board, Current_player).
+% -----------------------------------------------
+% 3. Display Board
+% -----------------------------------------------
 
-% Tabuleiro inicial
-board(1, [
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [empty, empty, b, b, b, b, b, b],
-    [empty, empty, b, b, b, b, b, b]
-]).
+% Display the entire board with column/row labels
+display_board(Board) :-
+    write('    0 1 2 3 4 5 6 7'), nl,
+    display_rows(Board, 0).
 
-% Variáveis do board
-item(b, 'Black').
-item(w, 'White').
-item(empty, 'Empty').
-
-% Variáveis do player
-player(1, player1).
-player(2, player2).
-
-% Variáveis da peça
-piece(player1, w).
-piece(player2, b).
-
-% Trocar de player
-change_player(player1, player2).
-change_player(player2, player1).
-
-% Selecionar o tabuleiro
-get_board(N, Board) :-
-    board(N, Board).
-
-% Display do item no tabuleiro
-display_item(Item) :-
-    item(Item, C),
-    write(C), write(' ').
-
-% Display de uma linha do tabuleiro
-display_row([]) :- nl.
-display_row([Item|RemainingItems]) :-
-    display_item(Item),
-    display_row(RemainingItems).
-
-% Display do tabuleiro
-display_board([]).
-display_board([Row|RemainingRows]) :-
+% Recursively print each row with index
+display_rows([], _).
+display_rows([Row|Remaining], RowIndex) :-
+    write(' '), write(RowIndex), write('  '),
     display_row(Row),
-    display_board(RemainingRows).
+    nl,
+    NextRow is RowIndex + 1,
+    display_rows(Remaining, NextRow).
 
-% Display do jogador atual
-display_player(Player) :-
-    player(_, PlayerName),
-    write('Current Player: '), write(PlayerName), nl.
+% Display a single row
+display_row([]).
+display_row([Cell|Cells]) :-
+    symbol(Cell, S),
+    write(S), write(' '),
+    display_row(Cells).
 
-% Obter uma peça no tabuleiro
+% -----------------------------------------------
+% 4. Display Current Player
+% -----------------------------------------------
+
+display_player(player1) :-
+    write('Current Player: White'), nl.
+
+display_player(player2) :-
+    write('Current Player: Black'), nl.
+
+% -----------------------------------------------
+% 5. Utility Predicates
+% -----------------------------------------------
+
+% Get n-th element from a list (0-based index)
+nthX([Head|_], 0, Head) :- !.
+nthX([_|Tail], N, Value) :-
+    N > 0,
+    N1 is N - 1,
+    nthX(Tail, N1, Value).
+
 get_piece(Board, X, Y, Piece) :-
-    nth0(Y, Board, Line),
-    nth0(X, Line, Piece).
+    nthX(Board, Y, Row),
+    nthX(Row, X, Piece).
 
-% Verificar se um movimento é válido
-valid_move(Board, X-Y) :- 
+valid_move(Board, X-Y) :-
     get_piece(Board, X, Y, empty).
 
-% Escolher uma jogada válida
-choose_move(Board, X-Y) :-
-    length(Board, N),
-    Max is N - 1,
-    repeat,
-    get_number(0, Max, 'Coordinate X', X), 
-    get_number(0, Max, 'Coordinate Y', Y),
-    valid_move(Board, X-Y), 
-    !.
+% -----------------------------------------------
+% 6. Put Piece - Updating the Board
+% -----------------------------------------------
 
-% Colocar uma peça no tabuleiro
+% Retrieve the element at position Index (0-based) from a list,
+% returning that element in Elem and the remaining list in Rest.
+
+% Base case
+nth0(0, [Elem|Rest], Elem, Rest).
+
+% Recursive case
+nth0(Index, [Head|Tail], Elem, [Head|NewTail]) :-
+    Index > 0,
+    Next is Index - 1,
+    nth0(Next, Tail, Elem, NewTail).
+
+% Example usage of nth0/4 in put_piece/4:
 put_piece(Board, X-Y, Piece, NewBoard) :-
-    nth0(Y, Board, Line, RestLines),
-    nth0(X, Line, _, RestItems),
-    nth0(X, NewLine, Piece, RestItems),
-    nth0(Y, NewBoard, NewLine, RestLines).
+    nth0(Y, Board, OldRow, RestRows),
+    nth0(X, OldRow, _, OldRowRest),
+    nth0(X, NewRow, Piece, OldRowRest),
+    nth0(Y, NewBoard, NewRow, RestRows).
 
-% Get a number from the user within a range
-get_number(Min, Max, Prompt, Number) :-
+% -----------------------------------------------
+% 7. Game Loop
+% -----------------------------------------------
+
+% Prompt the user for a coordinate in the range 0..7
+get_number(Min, Max, Prompt, N) :-
     repeat,
     write(Prompt), write(': '),
-    read(Number),
-    integer(Number),
-    Number >= Min,
-    Number =< Max,
+    read(N),
+    integer(N),
+    N >= Min,
+    N =< Max,
     !.
 
-% Menu inicial
+choose_move(Board, SrcX-SrcY, DestX-DestY) :-
+    length(Board, Size),
+    Max is Size - 1,
+    get_number(0, Max, 'Source X', SrcX),
+    get_number(0, Max, 'Source Y', SrcY),
+    get_number(0, Max, 'Destination X', DestX),
+    get_number(0, Max, 'Destination Y', DestY),
+    valid_move(Board, DestX-DestY),
+    !.
+
+game_loop((Board, CurrentPlayer)) :-
+    display_board(Board),
+    display_player(CurrentPlayer),
+    choose_move(Board, SrcX-SrcY, DestX-DestY),
+    piece(CurrentPlayer, Piece),
+    put_piece(Board, SrcX-SrcY, empty, TempBoard),
+    put_piece(TempBoard, DestX-DestY, Piece, NewBoard),
+    change_player(CurrentPlayer, NextPlayer),
+    game_loop((NewBoard, NextPlayer)).
+
+% -----------------------------------------------
+% 8. Menu
+% -----------------------------------------------
+
 play :-
-    get_board(1, Board),
+    board(1, Board),
     game_loop((Board, player1)).
-
-% Loop do jogo
-game_loop((Current_board, Current_player)) :-
-    display_board(Current_board),
-    display_player(Current_player),
-    choose_move(Current_board, X-Y),
-    piece(Current_player, Piece),
-    put_piece(Current_board, X-Y, Piece, New_board),
-    change_player(Current_player, New_player),
-    game_loop((New_board, New_player)).
-
-
 
 menu :-
     write('Welcome to Storm Clouds!'), nl,
@@ -121,16 +127,19 @@ menu :-
 menu_choice(1) :- play.
 menu_choice(2) :- rules, menu.
 menu_choice(3) :- write('Goodbye!'), nl.
-
+menu_choice(_) :-
+    write('Invalid choice!'), nl,
+    menu.
 
 rules :-
     write('Storm Clouds Rules:'), nl,
     write('- The game is played on an 8x8 board.'), nl,
     write('- Each player begins with 12 pieces.'), nl,
-    write('- Non-capturing moves (1 tile) - Black moves up, diagonal-up-right, right, diagonal-down-right.'), nl,
-    write('- Non-capturing moves (1 tile) - White moves up, diagonal-up-left, right, diagonal-up-right.'), nl,
-    write('- Capturing moves (multiple tiles) - Black moves down, diagonal-up-left, left, diagonal-down-left.'), nl,
-    write('- Capturing moves (multiple tiles) - White moves down, diagonal-down-right, left, diagonal-down-left.'), nl,
-    write('- The objective is to eliminate all opponent\'s pieces.'), nl,
-    write('- If a player cannot move any piece, they must pass the turn.'), nl.
-
+    write('- Non-capturing moves (1 tile):'), nl,
+    write('  * Black moves up, diagonal-up-right, right, diagonal-down-right.'), nl,
+    write('  * White moves up, diagonal-up-left, right, diagonal-up-right.'), nl,
+    write('- Capturing moves (multiple tiles):'), nl,
+    write('  * Black moves down, diagonal-up-left, left, diagonal-down-left.'), nl,
+    write('  * White moves down, diagonal-down-right, left, diagonal-down-left.'), nl,
+    write('- The objective is to eliminate all opponent pieces.'), nl,
+    write('- If a player cannot move any piece, they must pass.'), nl.
