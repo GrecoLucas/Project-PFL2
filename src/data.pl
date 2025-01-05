8c8584d427c616cda6a6759c982de3a178c0780d
GrecoLucas <up202208296@up.pt>
2025-01-05 16:25:08 +0000

    Updated data

--- src/data.pl
+++ src/data.pl
@@ -1,28 +1,8 @@
 % -----------------------------------------------
-% Dynamic Predicates
-% -----------------------------------------------
-
-% board/2 is dynamic because the board can change during the game (Bots, Players, etc.)
-:- dynamic board/2.
-
-
-% -----------------------------------------------
 % Board
 % -----------------------------------------------
 
-% Actual board, where the game is played
-board(1, [
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [w, w, empty, empty, empty, empty, empty, empty],
-    [empty, empty, b, b, b, b, b, b],
-    [empty, empty, b, b, b, b, b, b]
-]).
-
-% Initial board (Immutable board, template to reset the board)
+% Initial board configuration
 initial_board([
     [w, w, empty, empty, empty, empty, empty, empty],
     [w, w, empty, empty, empty, empty, empty, empty],
